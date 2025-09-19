f243335ecc6e89fdbcf734d3f0e69cd75242ed0b
Ramon <ramon.fc17@gmail.com>
2025-09-19 16:02:27 -0300

    Aprimora a listagem de alunos e ajusta a formatação de data no console

--- Program.cs
+++ Program.cs
@@ -1,14 +1,81 @@
 ﻿using System.Diagnostics.Tracing;
+using System.Globalization;
 using EXEMPLOEXPLORANDO.Models;
+System.Globalization.CultureInfo.DefaultThreadCurrentCulture = new System.Globalization.CultureInfo("en-US");
 
-Pessoa pessoa1 = new Pessoa(nome: "Ramon", sobrenome: "Costa");
 
-Pessoa pessoa2 = new Pessoa(nome:"bruce", sobrenome:"wayne");
+DateTime dataAtual = DateTime.Now;
 
-Curso cursodeIngles = new Curso();
-cursodeIngles.Nome = "Inglês";
-cursodeIngles.Alunos = new List<Pessoa>();
+Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm"));
 
-cursodeIngles.AdicionarAluno(pessoa1);
-cursodeIngles.AdicionarAluno(pessoa2);
-cursodeIngles.ListarAlunos();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//decimal ValorMonetario = 1582.40m;
+
+//Console.WriteLine(ValorMonetario.ToString("C2"));
+
+//double porcentagem = .3421;
+
+//Console.WriteLine(porcentagem.ToString("P1"));
+
+
+//int numero = 123456;
+//Console.WriteLine(numero.ToString("##-##-##"));
+
+
+
+
+
+
+
+
+
+
+//string numero1 = "10";
+//string numero2 = "20";
+
+//string resultado = numero1 + numero2;
+
+//Console.WriteLine(resultado);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Pessoa pessoa1 = new Pessoa(nome: "Ramon", sobrenome: "Costa");
+
+//Pessoa pessoa2 = new Pessoa(nome:"bruce", sobrenome:"wayne");
+
+//Curso cursodeIngles = new Curso();
+//cursodeIngles.Nome = "Inglês";
+//cursodeIngles.Alunos = new List<Pessoa>();
+
+///cursodeIngles.AdicionarAluno(pessoa1);
+//cursodeIngles.AdicionarAluno(pessoa2);
+//cursodeIngles.ListarAlunos();
